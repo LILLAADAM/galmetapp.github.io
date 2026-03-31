bf6a2773c034932b5799466c95a461d678e51a58
LILLAADAM <jsrajpooty@gmail.com>
2026-03-31 15:18:37 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,17 @@
 // firebase.js
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAxN1Q9g2kcDcaiTwPsSZEgsTvtyLyN3oY",
   authDomain: "galmet-app.firebaseapp.com",
   projectId: "galmet-app",
-  storageBucket: "galmet-app.firebasestorage.app",
+  storageBucket: "galmet-app.appspot.com",
   messagingSenderId: "900590512024",
   appId: "1:900590512024:web:16a36a085809a728c92ded"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+
+export { db };
